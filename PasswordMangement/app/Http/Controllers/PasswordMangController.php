fc6f6f597ba68dc3416fa9d9f62c87f2029e66d3
Alaa Salah <sbwjhkm@gmail.com>
2023-05-20 18:12:20 +0300

    up

---
+++ PasswordMangement/app/Http/Controllers/PasswordMangController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PasswordMang;
+use Illuminate\Http\Request;
+
+class PasswordMangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PasswordMang $passwordMang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PasswordMang $passwordMang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PasswordMang $passwordMang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PasswordMang $passwordMang)
+    {
+        //
+    }
+}
